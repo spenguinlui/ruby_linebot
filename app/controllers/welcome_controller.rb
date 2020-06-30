7e09f72b9d7a7a48cdcf156c3c1c68da4b7d7f9d
spenguinlui <spenguin10@yahoo.com.tw>
2020-06-30 22:08:49 +0800

    test

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,7 @@
 class WelcomeController < ApplicationController
   protect_from_forgery with: :null_session
   def webhook
-    render json: 'ok'
+    puts params["events"].first["message"]["text"]
+    # render json: 'ok'
   end
 end
