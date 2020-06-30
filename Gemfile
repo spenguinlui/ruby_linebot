0c2946d94027d6bf3586b6ac3060b392d6b43fff
spenguinlui <spenguin10@yahoo.com.tw>
2020-06-30 22:20:02 +0800

    add line-bot

--- Gemfile
+++ Gemfile
@@ -10,6 +10,9 @@ gem 'rails', '~> 6.0.3', '>= 6.0.3.2'
 
 # Postgres
 gem 'pg'
+
+# line-bot-api
+gem 'line-bot-api'
 # Use Puma as the app server
 gem 'puma', '~> 4.1'
 # Use SCSS for stylesheets
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,25 @@ class WelcomeController < ApplicationController
   protect_from_forgery with: :null_session
   def webhook
     puts params["events"].first["message"]["text"]
-    # render json: 'ok'
+
+    # 物件初始化
+    client = Line::Bot::Client.new { |config|
+      config.channel_secret = ENV['line_channel_secret']
+      config.channel_token = ENV['line_channel_token']
+    }
+
+    # 收到的訊息會以 params[:events] 傳回
+
+    # 加入 token
+    reply_token = params['events'][0]['replyToken']
+
+    # 回覆內容
+    response_message = {
+      type: "text",
+      text: params["events"].first["message"]["text"]
+    }
+
+    # 回覆
+    client.reply_message(reply_token, response_message)
   end
 end
